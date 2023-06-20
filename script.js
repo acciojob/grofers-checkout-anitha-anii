14ecf06dd03fc60e707c75cc6709548658191715
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-06-20 15:53:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,24 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
-};
+const table = document.querySelector('table');
+const prices = document.querySelectorAll('.price');
+
+let totalPrice = 0;
+for (let i = 0; i < prices.length; i++) {
+  totalPrice += parseFloat(prices[i].innerText);
+}
+
+const newRow = document.createElement('tr');
+const totalCell = document.createElement('td');
+	
+totalCell.setAttribute('colspan', '2');
+totalCell.innerText = 'Total Price: ' + totalPrice.toFixed(2); // Format the total price with 2 decimal places
+
+newRow.appendChild(totalCell);
+table.appendChild(newRow);
+
+	};
 
 getSumBtn.addEventListener("click", getSum);
 
